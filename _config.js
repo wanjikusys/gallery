d612ed09754b01792a15ee4382f7fa7515a4edee
wanjikusys <leewama01@gmail.com>
2025-04-08 22:23:41 +0300

    mongodb credentials update

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://Leena:EalSKyuHZW87Kv5V@lmcluster.ylnbngf.mongodb.net/?retryWrites=true&w=majority&appName=LMCluster',
+    development: 'mongodb+srv://Leena:EalSKyuHZW87Kv5V@lmcluster.ylnbngf.mongodb.net/?retryWrites=true&w=majority&appName=LMCluster',
+    test: 'mongodb+srv://Leena:EalSKyuHZW87Kv5V@lmcluster.ylnbngf.mongodb.net/?retryWrites=true&w=majority&appName=LMCluster',
 }
 module.exports = config;
