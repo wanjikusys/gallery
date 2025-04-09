d79a5fafd5e31241752958837aaa233e14f36655
wanjikusys <leewama01@gmail.com>
2025-04-09 22:21:17 +0300

    updated mongobd credentials

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://Leena:EalSKyuHZW87Kv5V@lmcluster.ylnbngf.mongodb.net/?retryWrites=true&w=majority&appName=LMCluster',
-    development: 'mongodb+srv://Leena:EalSKyuHZW87Kv5V@lmcluster.ylnbngf.mongodb.net/?retryWrites=true&w=majority&appName=LMCluster',
-    test: 'mongodb+srv://Leena:EalSKyuHZW87Kv5V@lmcluster.ylnbngf.mongodb.net/?retryWrites=true&w=majority&appName=LMCluster',
+    production: 'mongodb+srv://Leena:Leena2025!@lmcluster.ylnbngf.mongodb.net/?retryWrites=true&w=majority&appName=LMCluster',
+    development: 'mongodb+srv://Leena:Leena2025!@lmcluster.ylnbngf.mongodb.net/?retryWrites=true&w=majority&appName=LMCluster',
+    test: 'mongodb+srv://Leena:Leena2025!@lmcluster.ylnbngf.mongodb.net/?retryWrites=true&w=majority&appName=LMCluster',
 }
 module.exports = config;
